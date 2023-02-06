80239cdd156b4f41a7bd0de082762e63c348d354
DESKTOP-G85SRSM\hwlsg <hjhlove1120@gmail.com>
2023-02-06 20:01:15 +0900

    faet : initiate project

---
+++ src/test/java/com/zerobase/carrot_auction/CarrotAuctionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zerobase.carrot_auction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarrotAuctionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
