359be66e823d1bedd051cfbcbeb5ea58e21de5b1
JEONG <jungds95@naver.com>
2023-02-26 22:00:25 +0900

    fix(errorcode) : fix2 ErrorCode

--- src/main/java/com/zerobase/carrot_auction/exception/ErrorCode.java
+++ src/main/java/com/zerobase/carrot_auction/exception/ErrorCode.java
@@ -13,7 +13,7 @@ public enum ErrorCode {
 	NEGATIVE_END_PERIOD(HttpStatus.BAD_REQUEST, "기간이 0이하일 수 없습니다."),
 	NOT_FOUND_PRODUCT(HttpStatus.NOT_FOUND, "거래글을 찾을 수 없습니다."),
 	NOT_FOUND_DEAL(HttpStatus.NOT_FOUND, "거래 정보를 찾을 수 없습니다."),
-	NOT_FOUND_USER(HttpStatus.BAD_REQUEST, "유저를 찾을 수 없습니다."),
+	NOT_FOUND_USER(HttpStatus.NOT_FOUND, "유저를 찾을 수 없습니다."),
 	INVALID_USER(HttpStatus.BAD_REQUEST, "판매자 정보가 일치하지 않습니다."),
 	INVALID_PRICE(HttpStatus.BAD_REQUEST, "입력한 가격이 입찰가보다 낮습니다"),
 	INVALID_PRODUCT_STATUS(HttpStatus.BAD_REQUEST, "판매중인 거래만 종료할 수 있습니다."),
